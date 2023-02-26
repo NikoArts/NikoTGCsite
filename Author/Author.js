e51e5abde51588b7a4e04785457b45d51cd2da2c
Niko  ❮Arts❯ <104503751+NikoArts@users.noreply.github.com>
2023-02-26 22:06:04 +0300

    Add files via upload

---
+++ Author/Author.js
@@ -0,0 +1,300 @@
+function N0()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko0.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko0.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Niko_❮Arts❯ </h3></a> <h3> <li class="AuthorInfo2"> Имя: Николай (eng: Nikolai) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Автор <br> Раса: Человек <br><br><a class="Selected">Информация об авторе: </a><br><a class="Selected">*</a>Обычный глючный художник.<br><a class="Selected">*</a>Автор всех вселенных "Глючной мусорки".<br><a class="Selected">*</a>Создатель этого сайта.<br><a class="Selected">*</a>Увлекается рисованием и созданием вселенных.<br><a class="Selected">*</a>Токсичный клоун лол.<br></li></h3></p></a>  ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N1()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko1.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko1.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Nико </h3></a> <h3> <li class="AuthorInfo2"> Имя: Нико (eng: Niko) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Основной персонаж Нико.<br> <a class="Selected">*</a>Воплощение автора и всех основных черт личности и характера.<br>  <a class="Selected">*</a>Занимает Нейтралитет в фракциях разных версий нико (Фракция Порядка, и Фракция Хаоса).<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N2()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko2.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko2.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Никки </h3></a> <h3> <li class="AuthorInfo2"> Имя: Никки (eng: Nikki) <br> Возраст: 19 лет <br> Пол: Женский <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br><a class="Selected">*</a>Основная Фем. версия Нико.<br><a class="Selected">*</a>Воплощение: Спокойствия, рассудительности и адекватности. <br>  <a class="Selected">*</a>Часто раздражается.<br>  <a class="Selected">*</a>ОЧЕНЬ негативна в сторону Глича.<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N3()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko3.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko3.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Глич </h3></a> <h3> <li class="AuthorInfo2"> Имя: Глич (eng: Glitch) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Тёмная версия личности Нико.<br> <a class="Selected">*</a>Воплощение: Хаоса, жестокости, безумия, маниакальности и садизма.<br>  <a class="Selected">*</a>Заключён в "Запретной зоне разума" (Пример в старой версии комикса "GlitchSlimes Adventure").<br>  <a class="Selected">*</a>Если описывать двумя словами то можно назвать его "Безумным говнюком".<br>  <a class="Selected">*</a>Имеет слабость к Нишики.<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N4()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko4.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko4.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Нишики </h3></a> <h3> <li class="AuthorInfo2"> Имя: Нишики (eng: Nishiki) <br> Возраст: 19 лет <br> Пол: Женский <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Вторая Fem!Версия Нико.<br> <a class="Selected">*</a>Воплощение: Страха, Отчаяния, Само-ненависти, скромности и боли.<br>  <a class="Selected">*</a>Очень пугливая и добрая.<br>  <a class="Selected">*</a>Имеет чувства к Гличу.<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N5()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko5.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko5.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Фувани </h3></a> <h3> <li class="AuthorInfo2"> Имя: Фувани (eng: Fuwani) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Одним словом, Шиппер.<br> <a class="Selected">*</a>Воплощение: Счастья, Доброты, Энергичности, Мотивации и романтичности.<br>  <a class="Selected">*</a>Обожает романтические истории и сюжеты.<br>  <a class="Selected">*</a>Если в романтическом произведении персонажи наконец то держаться за руки/ Обнимаются или целуются, то может сломать стену от счастья (ну или спародировать мем про чуваков на диване которые радуются чему-то).<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N6()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko6.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko6.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Муко </h3></a> <h3> <li class="AuthorInfo2"> Имя: Муко (eng: Muko) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Музыкант (Хоть и плох в этом).<br> <a class="Selected">*</a>Воплощение: Задорности, Ритмичности, Позитивной загадочности и мелодичности.<br> <a class="Selected">*</a>Никогда не открывает глаза.<br> <a class="Selected">*</a>Иногда может быть загадочнее Вондера.<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N7()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko7.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko7.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Дарк </h3></a> <h3> <li class="AuthorInfo2"> Имя: Дарк!Нико (eng: Dark!Niko) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Хранитель всего милого и невинного.<br> <a class="Selected">*</a>Воплощение: Справедливости, Беспощадности, Силы, и Решимости надрать чей-то зад.<br> <a class="Selected">*</a>Часто хрустит пальцами.<br> <a class="Selected">*</a>Очень по доброму относиться ко всему пушистому.<br>  </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N8()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko8.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko8.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> BSOD </h3></a> <h3> <li class="AuthorInfo2"> Имя: BSOD!Нико <br>(eng: BSOD!Niko) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>ОШИБКА 404.<br> <a class="Selected">*</a>Воплощение: Шока, Непонимания, Очень долгого мышления и просто лагов (как в голове так и техники).<br>  <a class="Selected">*</a>ВРЕМЯ ПЕРЕУСТАНАВЛИВАТЬ ШИНДОУС!!!.<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N9()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko9.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko9.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Офицер Ник </h3></a> <h3> <li class="AuthorInfo2"> Имя: Хорни-Офицер Ник <br>(eng: Horny police Nick) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Начальник Хорни-тюрьмы.<br> <a class="Selected">*</a>Воплощение: Порядочности, Невинности, Лёгкой упоротости и честности.<br>  <a class="Selected">*</a>Вы хорни и спермотоксикозник? НЕ БЕДА! У нас для вас есть камера ;D .<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N10()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko10.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko10.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Арико </h3></a> <h3> <li class="AuthorInfo2"> Имя: Арико (eng: Ariko) <br> Возраст: 19 лет <br> Пол: Женский <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Очень ленивая художница.<br> <a class="Selected">*</a>Воплощение: Креативности, Оригинальности, Воображения, Творчества, Внимательности и Умиротворения.<br>  <a class="Selected">*</a>Когда устаёт часто засыпает на коленях Дарка (ну или засыпает обнимая Дарка).<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N11()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko11.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko11.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Прототип </h3></a> <h3> <li class="AuthorInfo2"> Имя: Прототип!Нико <br>(eng: Prototype!Niko) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Прототип Нико.<br> <a class="Selected">*</a>Воплощение: Зависти, Хладнокровия, Внимания и кровожадности.<br>  <a class="Selected">*</a>Лидер Фракции Хаоса.<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N12()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko12.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko12.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Меха </h3></a> <h3> <li class="AuthorInfo2"> Имя: Меха_НК_3 <br>(eng: Mecha_NK_3) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Андроид <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Механическая копия Нико.<br> <a class="Selected">*</a>Воплощение: Механичности, По очерёдности, Распорядительности, Автоматичности и Послушности.<br> <a class="Selected">*</a>Готов на всё что бы заменить собою Оригинального Нико.<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N13()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko13.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko13.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Ханикки </h3></a> <h3> <li class="AuthorInfo2"> Имя: Ханикки (eng: Hunnikki) <br> Возраст: 19 лет <br> Пол: Женский <br> Тип: Персонаж <br> Раса: Глючный Слайм <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Обжора.<br> <a class="Selected">*</a>Воплощение: Чревоугодия, Голода, Жадности, Безумия, Эйфории, Ненасытности, Жажды и Слабой силы воли.<br> <a class="Selected">*</a>в 90% случаев возбужденна(НЕ В СЕКСУАЛЬНОМ ПЛАНЕ!!!) из за мыслей о разных вкусностях.<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+function N14()
+
+{
+setTimeout(function(){
+AuthorIMG.src='Images/Author/Niko14.png';
+AuthorIMG2.src='Images/Author/Chibi/SmolNiko14.png';
+
+document.getElementById("NikoInfo").innerHTML = ' <p align="left"> <a><h3> Вондер </h3></a> <h3> <li class="AuthorInfo2"> Имя: Wonderful idea!Нико <br> (eng: Wodnerful idea!Niko) <br> Возраст: 19 лет <br> Пол: Мужской <br> Тип: Персонаж <br> Раса: Глючный Слайм????? <br><br><a class="Selected">Информация об персонаже: </a><br> <a class="Selected">*</a>Любитель сделок и договоров.<br> <a class="Selected">*</a>Воплощение: Загадочности, Секретности, Игривости, Лживости, Хитрости и Скрытности.<br> <a class="Selected">*</a>Никто не знает кто он, откуда, и что у него под маской.<br> </li></h3></p></a> ';
+},1);
+
+document.getElementById('AuthorIMG').classList.add('Bouncy');
+document.getElementById('AuthorIMG2').classList.add('Bouncy');
+setTimeout(function(){
+document.getElementById('AuthorIMG').classList.remove('Bouncy');
+document.getElementById('AuthorIMG2').classList.remove('Bouncy');
+},500);
+}
+
+//Ripple Event Handler
+var drawRipple = function(ev) {
+  var x = ev.clientX;
+  var y = ev.clientY;
+  var node = document.querySelector(".ripple");
+  var newNode = node.cloneNode(true);
+  newNode.classList.add("animate");
+  newNode.style.left = ev.clientX - 5 + "px";
+  newNode.style.top = ev.clientY - 5 + "px";
+  node.parentNode.replaceChild(newNode, node);
+};
+
+//Ripple Triggers
+window.addEventListener("click", drawRipple);
+
+
+//Control Handler
+var controlHandler = function() {
+  document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    controller.textContent = "Dark Ripple";
+  } else {
+    controller.textContent = "Bright Ripple";
+  }
+};
+
+// Control Trigger
+var controller = document.querySelector(".controller");
+controller.addEventListener("click", controlHandler);
+
