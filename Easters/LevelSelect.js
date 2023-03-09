28c0b4ccb1e2e8658c9d7ecd5f318755fa7d88af
Niko  ❮Arts❯ <104503751+NikoArts@users.noreply.github.com>
2023-03-09 19:24:57 +0300

    Add files via upload

---
+++ Easters/LevelSelect.js
@@ -0,0 +1,46 @@
+function l1(event){
+  L1.src='LevelSelect/Levels/Index.png';
+}
+
+function l2(event){
+  L1.src='LevelSelect/Levels/Password.png';
+}
+
+function l3(event){
+  L1.src='LevelSelect/Levels/WIP.png';
+}
+
+function l4(event){
+  L1.src='LevelSelect/Levels/Universes.png';
+}
+
+function l5(event){
+  L1.src='LevelSelect/Levels/WIP.png';
+}
+
+function l6(event){
+  L1.src='LevelSelect/Levels/Asks.png';
+}
+
+function l7(event){
+  L1.src='LevelSelect/Levels/Comics.png';
+}
+
+function l8(event){
+  L1.src='LevelSelect/Levels/Changes.png';
+}
+
+function l9(event){
+  L1.src='LevelSelect/Levels/Author.png';
+}
+
+function l10(event){
+  L1.src='LevelSelect/Levels/MineCraft.png';
+}
+
+function l11(event){
+  L1.src='LevelSelect/Levels/LevelSelect.png';
+}
+
+var audio = document.getElementById("myaudio");
+			audio.volume = 0.2;
