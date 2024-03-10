1f51483cbfa3464b03d58fd8c0b7b52a7b043a1f
Niko  ❮Arts❯ <104503751+NikoArts@users.noreply.github.com>
2024-03-10 15:57:02 +0300

    Add files via upload

--- SetupJs.js
+++ SetupJs.js
@@ -16,7 +16,7 @@ document.getElementById('Fade').className = "FadeOut";
 setTimeout(function()
 
 {
-document.getElementById("Setup").innerHTML = '<img src = "Images/Icons/Github.png" class="Beat4" height="20%" style="position: fixed; top: 10vw; left: 20vw; "><h4 class="Beat6" style="position: fixed; top: 5vw; left: 30vw;  text-align: left;"> Hosting: Github </h4><h2 class="Beat6" style="position: fixed; top: 15vw; left: 30vw;  text-align: left;"> https://github.com/ </h2><img src = "Images/Icons/FreeNom.png" class="Beat4" height="20%" style="position: fixed; bottom: 10vw; right: 20vw; "><h4 class="Beat6" style="position: fixed; bottom: 10vw; right: 30vw; text-align: right;"> Domen: FreeNom </h4><h2  class="Beat6"style="position: fixed; bottom: 10vw; right: 30vw; text-align: right;"> https://www.freenom.com/ </h2>';
+document.getElementById("Setup").innerHTML = '<img src = "Images/Icons/Github.png" class="Beat4" height="20%" style="position: fixed; top: 10vw; left: 20vw; "><h4 class="Beat6" style="position: fixed; top: 5vw; left: 30vw;  text-align: left;"> Hosting: Github </h4><h2 class="Beat6" style="position: fixed; top: 15vw; left: 30vw;  text-align: left;"> https://github.com/ </h2><img src = "Images/Icons/FreeDNS.png" class="Beat4" height="20%" style="position: fixed; bottom: 10vw; right: 20vw; "><h4 class="Beat6" style="position: fixed; bottom: 10vw; right: 30vw; text-align: right;"> Domen: FreeDNS </h4><h2  class="Beat6"style="position: fixed; bottom: 10vw; right: 30vw; text-align: right;"> https://freedns.afraid.org/ </h2>';
 document.getElementById('Fade').className = "FadeIn";
 }
 ,7224);
@@ -33,7 +33,7 @@ document.getElementById('Fade').className = "FadeOut";
 setTimeout(function()
 
 {
-document.getElementById("Setup").innerHTML = '<h2> Для лучшего восприятия включите функцию Autoplay в браузере </h2>';
+document.getElementById("Setup").innerHTML = '<h1> Для лучшего восприятия включите функцию Autoplay в браузере </h1>';
 document.getElementById('Fade').className = "FadeIn";
 }
 ,12040);
