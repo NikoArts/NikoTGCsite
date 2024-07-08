4d0f1601cb49ab54b2c1f03c7d723ed2d73a9312
Niko  ❮Arts❯ <104503751+NikoArts@users.noreply.github.com>
2024-07-09 01:10:43 +0300

    Add files via upload

--- Credits/Credits Theme.js
+++ Credits/Credits Theme.js
@@ -1,7 +1,20 @@
-var Audio = new Audio('Credits.ogg'); 
+var songs = ['Credits.ogg', 'PasswordCredits.ogg', 'MenuThemeCredits.ogg', 'oh no! ERROR! Credits.ogg', 'AuthorCredits.ogg'];
+var currentSongIndex = 0;
+var audio = new Audio(songs[currentSongIndex]);
 
-window.addEventListener('load', function (){
-    Audio.play(); 
-	Audio.loop = true;
-	Audio.muted = false;
+window.addEventListener('load', function () {
+    playSong();
+    audio.muted = false;
 });
+
+audio.addEventListener('ended', function () {
+    currentSongIndex++;
+    if (currentSongIndex < songs.length) {
+        playSong();
+    }
+});
+
+function playSong() {
+    audio.src = songs[currentSongIndex];
+    audio.play();
+}
